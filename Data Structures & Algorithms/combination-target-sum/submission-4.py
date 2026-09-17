9ef50f998a48131d690631f303352389a8caf1c5
Nikhil Hooda <87490556+NikhilHooda@users.noreply.github.com>
2026-09-16 17:00:50 -0700

    Add: combination-target-sum - submission-4

---
+++ Data Structures & Algorithms/combination-target-sum/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        #base case: sum == target
+        #choice: all idxs (including own) and above (nothing behind)
+        #constraint, if sum + new_addition >= target then continue
+        ans = []
+        n = len(nums)
+        nums.sort()
+
+        def backtrack(idx, path, total):
+            if total == target:
+                ans.append(path[:])
+                return
+            
+            for j in range(idx, n):
+                new_total = total + nums[j]
+                if new_total > target:
+                    break
+                path.append(nums[j])
+                backtrack(j, path, new_total)
+                path.pop()
+
+        backtrack(0, [], 0)
+        return ans
